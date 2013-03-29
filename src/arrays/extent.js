a5c773d891a8f0c46bb071019e5b3818edf6c098
JackLee <freefoxser@gmail.com>
2013-03-29 20:47:05 +0800

    ?

--- src/arrays/extent.js
+++ src/arrays/extent.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // 返回数组中所有元素最小和最大值
 d3.extent = function (array, f) {
     var i = -1,
@@ -20,3 +21,26 @@ d3.extent = function (array, f) {
     }
     return [a, c];
 };
+=======
+d3.extent = function (array, f) {
+    var i = -1,
+        n = array.length,
+        a,
+        b,
+        c;
+    if (arguments.length === 1) {
+        while (++i < n && ((a = c = array[i]) == null || a != a)) a = c = undefined;
+        while (++i < n) if ((b = array[i]) != null) {
+            if (a > b) a = b;
+            if (c < b) c = b;
+        }
+    } else {
+        while (++i < n && ((a = c = f.call(array, array[i], i)) == null || a != a)) a = undefined;
+        while (++i < n) if ((b = f.call(array, array[i], i)) != null) {
+            if (a > b) a = b;
+            if (c < b) c = b;
+        }
+    }
+    return [a, c];
+};
+>>>>>>> ?
