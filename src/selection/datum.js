4b73e64529b37838736facb0865f8ab012653205
JackLee <freefoxser@gmail.com>
2013-04-03 19:05:43 +0800

    more example

--- src/selection/datum.js
+++ src/selection/datum.js
@@ -1,7 +1,14 @@
-import "selection";
+import
+"selection";
 
-d3_selectionPrototype.datum = function(value) {
-  return arguments.length
-      ? this.property("__data__", value)
-      : this.property("__data__");
+/**
+ * 直接设置展示对象的数据, 这在已有展示对象, 初始化对象时候有用
+ *
+ * @param value
+ * @returns {*}
+ */
+d3_selectionPrototype.datum = function (value) {
+    return arguments.length
+        ? this.property("__data__", value)
+        : this.property("__data__");
 };
---
+++ tools/lib/widget.js
@@ -0,0 +1,56 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: droidhen69
+ * Date: 13-4-3
+ * Time: 上午10:00
+ * To change this template use File | Settings | File Templates.
+ */
+function Selection(root, style) {
+    this.root = root;
+    this.style = style;
+}
+Selection.prototype.select = function() {
+    return this.root.selectAll('.' + this.style);
+}
+/**
+ * controller
+ *
+ * @param selector
+ * @constructor
+ */
+function Widget(selector) {
+    this.x = 0;
+    this.y = 0;
+    this.selector = selector;
+    this.data = {};
+}
+Widget.prototype.bind = function(data) {
+    var context = this.selector.select().data([this.data]);
+
+    var widget = this;
+    context.enter().append('svg:g').attr('class', this.selector.style).each(function(d, i, nindex) {
+        widget.enterUI(this, d, i, nindex);
+    });
+    context.each(function(d, i, nindex) {
+        widget.updateUI(this, d, i, nindex);
+    });
+    context.exit().each(function(d, i, nindex) {
+        widget.exitUI(this, d, i, nindex);
+    });
+}
+Widget.prototype.enterUI = function(ui, index) {
+}
+Widget.prototype.updateUI = function(ui) {
+}
+Widget.prototype.exitUI = function(ui) {
+    d3.select(ui).remove();
+}
+Widget.enter = function(d, i, nindex) {
+    d.enterUI(this, d, i, nindex);
+}
+Widget.update = function(d, i, nindex) {
+    d.updateUI(this, d, i, nindex);
+}
+Widget.exit = function(d, i, nindex) {
+    d.exitUI(this, d, i, nindex);
+}
