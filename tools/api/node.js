669a270647a3979c21800a1167054195f1f7d46a
JackLee <freefoxser@gmail.com>
2013-04-23 00:06:41 +0800

    listener adapter

--- tools/api/node.js
+++ tools/api/node.js
@@ -1,3 +1,139 @@
+function selection(node) {
+    this.node = node;
+}
+selection.prototype.append = function (name) {
+
+}
+selection.prototype.__create_adapter__ = function () {
+    var fns = [];
+
+    function add(declare) {
+        for (var i = 0; i < fns.length; i++) {
+            var f = fns[i];
+            if (f.name == declare.name) {
+                return;
+            }
+        }
+        fns.push(declare);
+    }
+
+    function remove(name) {
+        for (var i = 0; i < fns.length; i++) {
+            var f = fns[i];
+            if (f.name == name) {
+                fns.splice(i, 1);
+                return;
+            }
+        }
+    }
+
+    function listener(event) {
+        for (var i = 0; i < fns.length; i++) {
+            var f = fns[i];
+            if (f.bind) {
+                f.fn.call(bind, event, this);
+            } else {
+                f.fn.call(this, event, this);
+            }
+        }
+    }
+
+    function count() {
+        return fns.length;
+    }
+
+    listener.add = add;
+    listener.remove = remove;
+    listener.count = count;
+
+    return listener;
+}
+selection.prototype.on = function (event, listener, capture, bind) {
+    if (typeOf(listener) != 'function' || typeOf(event) != 'string') {
+        return;
+    }
+
+    var events;
+    if (!this.node.__events__) {
+        events = this.node.__events__ = {};
+    } else {
+        events = this.node.__events__;
+    }
+
+    var type = event;
+    var name = '';
+    var i = event.indexOf(".");
+    if (i > 0) {
+        type = event.substring(0, i);
+        name = event.substring(i + 1);
+    }
+
+    if (arguments.length == 3) {
+        if (typeOf(capture) != 'boolean') {
+            bind = capture || null;
+            capture = false;
+        } else {
+            bind = null;
+        }
+    } else if (arguments.length == 4) {
+        if (typeOf(capture) != 'boolean') {
+            capture = false;
+        }
+        if (!bind) {
+            bind = null;
+        }
+    }
+
+    var key = type + (capture ? '_c' : '');
+    var pool;
+    if (!(pool = events[key])) {
+        pool = events[key] = this.__create_adapter__();
+        this.node.addEventListener(type, pool, capture);
+    }
+    pool.add({name: name, fn: listener, bind: bind});
+    return this;
+}
+selection.prototype.off = function (event) {
+    if (typeOf(event) != 'string') {
+        return;
+    }
+    var events;
+    if (!(events = this.node.__events__)) {
+        return;
+    }
+
+    var type = event;
+    var name = '';
+    var i = event.indexOf(".");
+    if (i > 0) {
+        type = event.substring(0, i);
+        name = event.substring(i + 1);
+    }
+
+    var lis;
+    if (lis = events[type]) {
+        lis.remove(name);
+        if (lis.count() == 0) {
+            this.node.removeEventListener(type, lis, false);
+        }
+        delete events[type];
+    }
+    if (lis = events[type + '_c']) {
+        lis.remove(name);
+        if (lis.count() == 0) {
+            this.node.removeEventListener(type, lis, true);
+        }
+        delete events[type + '_c'];
+    }
+    var count = 0;
+    for (i in events) {
+        count++;
+        break;
+    }
+    if (count == 0) {
+        delete this.node.__events__;
+    }
+}
 /**
  * Created with JetBrains WebStorm.
  * User: jie
